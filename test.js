97a996acc66447d509384f667b184136c5e6de65
Roman <starsetphenomenon@gmail.com>
2022-09-23 17:37:03 +0300

    fixed

--- test.js
+++ test.js
@@ -1,24 +1,29 @@
 function loadGrain(levels) {
     let counter = 0;
+    let min = 0;
+
+    if (levels[0] > levels[levels.length - 1]) {
+        min = levels[levels.length - 1]
+        levels.reverse()
+    } else {
+        min = levels[0];
+    }
+
     for (let i = 0; i < levels.length - 1; i++) {
-        let min = 0;
-        if ((levels[i - 1] && levels[i + 1]) !== undefined && (levels[i - 1] && levels[i + 1]) > levels[i]) {
-            if (levels[i - 1] > levels[i + 1]) {
-                min = levels[i + 1]
-                levels.length <= 3 ? counter += min - levels[i] : counter += levels[i - 1] - levels[i]
-                levels[i] += levels[i - 1] - levels[i]
-            } else {
-                min = levels[i - 1]
-                counter += min - levels[i]
-                levels[i] += min - levels[i]
-            }
+        if (levels[i] >= min) {
+            min = levels[i]
+        } else {
+            counter += min - levels[i]
+            levels[i] += min - levels[i]
         }
+
     }
     console.log(counter)
     return counter
 }
 
 loadGrain([4, 1, 3]) // 2
+loadGrain([39, 9, 39, 42, 22, 20, 2, 26, 22, 48, 45]) // 148
 loadGrain([2, 1, 5, 2, 7, 4, 10]) // 7
 loadGrain([2, 0, 1, 5, 2, 7]) // 6
 loadGrain([2, 4, 2]) // 0
